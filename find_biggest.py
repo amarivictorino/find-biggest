96636a24a3807193ba27a829f87d3329690a5c4b
Angelica Victorino <you@example.com>
2024-01-08 21:53:09 +0800

    find biggest

--- find_biggest.py
+++ find_biggest.py
@@ -1,22 +1,42 @@
 # Ask user for an input. Find the biggest number.
 
 # pseudocode
+# number = input("input a number")
+print("Enter the first number:")
+first_number_float = float(input())
+first_number_whole = round(first_number_float)
 
-# Ask user for an input
+print("Enter the second number:")
+second_number_float = float(input())
+second_number_whole = round(second_number_float)
 
-if second_number > first_number :
-  biggest_number = second_number
+if second_number_whole > first_number_whole:
+  biggest_number = second_number_whole
 else:
-  biggest_number = first_number
+  biggest_number = first_number_whole
 
-if third_number > biggest_number :
-  biggest_number = third_number
+print("Enter the third number:")
+third_number_float = float(input())
+third_number_whole = round(third_number_float)
 
-if fourth_number > biggest_number :
-  biggest_number = fourth_number
+if third_number_whole > biggest_number:
+  biggest_number = third_number_whole
 
-if fifth_number > biggest_number :
-  biggest_number = fifth_number
+print("Enter the fourth number:")
+fourth_number_float = float(input())
+fourth_number_whole = round(fourth_number_float)
+
+if fourth_number_whole > biggest_number:
+  biggest_number = fourth_number_whole
+
+print("Enter the fifth number:")
+fifth_number_float = float(input())
+fifth_number_whole = round(fifth_number_float)
+
+if fifth_number_whole > biggest_number:
+  biggest_number = fifth_number_whole
+
+print("The biggest whole number is:", biggest_number)
 
 
 
