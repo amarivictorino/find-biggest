dac7f861aad57ca8f7633c86138fe0a7e7aafbeb
Angelica Victorino <you@example.com>
2024-01-08 20:40:17 +0800

    pseudocode

---
+++ find_biggest.py
@@ -0,0 +1,24 @@
+# Ask user for an input. Find the biggest number.
+
+# pseudocode
+
+# Ask user for an input
+
+if second_number > first_number :
+  biggest_number = second_number
+else:
+  biggest_number = first_number
+
+if third_number > biggest_number :
+  biggest_number = third_number
+
+if fourth_number > biggest_number :
+  biggest_number = fourth_number
+
+if fifth_number > biggest_number :
+  biggest_number = fifth_number
+
+
+
+ 
+    
